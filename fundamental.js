fb4c9fbc79eb0c43c46f5a26019f4c8fc0a80ff7
husnifatah <husnifatah0@gmail.com>
2025-07-25 10:01:23 +0700

    first commit

---
+++ fundamental.js
@@ -0,0 +1,31 @@
+// sring untuk menyimpan tipe data text
+let c = "13214234";
+console.log(c);
+
+// number
+let h = 23;
+console.log(h);
+
+// boolean
+let sedangBelajar = false;
+console.log(typeof sedangBelajar);
+
+// array
+const hewan = ["ikan", "ayam", "sapi"];
+console.log(hewan[0]);
+
+// object
+const tanaman = { rumput: "jepang", berbuah: "matoa", berduri: "kaktus" };
+console.log(tanaman.rumput);
+
+alert("PAGIIII!!!!");
+
+let umurmu = prompt("Berapa Umurmu?");
+alert("Umurku, " + umurmu);
+
+const hapus = confirm("Apakah kamu yakin untuk menghapus?");
+if (hapus === true) {
+  alert("Sudah Terhapus, Yey!");
+} else {
+  alert("Tidak terhapus:(");
+}
